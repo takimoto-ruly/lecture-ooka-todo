24f598cbac16541f1bb337a1328c2b8b0620fcf4
ruly-ooka <ayana.ruly@outlook.jp>
2026-04-23 10:48:37 +0900

    FizzBuzzメソッドの追加

--- src/main/java/ruly/lecture/ooka/practice/basic/FizzBuzz.java
+++ src/main/java/ruly/lecture/ooka/practice/basic/FizzBuzz.java
@@ -2,7 +2,16 @@ package ruly.lecture.ooka.practice.basic;
 
 public class FizzBuzz {
 	public String convert(int n) {
+
+		if (n % 15 == 0) {
+			return "FizzBuzz";
+		} else if (n % 3 == 0) {
+			return "Fizz";
+		} else if (n % 5 == 0) {
+			return "Buzz";
+		}
 		// ここを実装してください
-		return "";
+		String s = String.valueOf(n);
+		return s;
 	}
 }
