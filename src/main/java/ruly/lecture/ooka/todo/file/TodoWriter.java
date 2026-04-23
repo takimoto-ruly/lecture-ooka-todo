018882e70caec74b1627b5b9544c0265142458eb
ruly-ooka <ayana.ruly@outlook.jp>
2026-04-23 09:32:12 +0900

    TODOWriterの追加

--- src/main/java/ruly/lecture/ooka/todo/file/TodoWriter.java
+++ src/main/java/ruly/lecture/ooka/todo/file/TodoWriter.java
@@ -1,5 +1,7 @@
 package ruly.lecture.ooka.todo.file;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import ruly.lecture.ooka.todo.dto.Todo;
@@ -8,9 +10,17 @@ public class TodoWriter {
 
 	/**
 	 * TODOリストをファイルに書き込み保存する
+	 * @throws IOException 
 	 */
-	
-	public void Write(List<Todo> todos) {
+
+	public void write(String filePath, List<Todo> todos) throws IOException {
+
+		try (FileWriter fw = new FileWriter(filePath)) {
+			for (Todo todo : todos) {
+				fw.write(todo.getId() + "(" + todo.getStatus().getMark() + ")" + todo.getContent());
+				fw.write(System.lineSeparator());
+			}
+		}
 
 	}
 }
---
+++ src/main/java/ruly/lecture/ooka/todo/logic/TodoDisplay.java
@@ -0,0 +1,16 @@
+package ruly.lecture.ooka.todo.logic;
+
+import java.util.List;
+
+import ruly.lecture.ooka.todo.dto.Todo;
+
+public class TodoDisplay {
+
+	/**
+	 * TODO一覧を表示する。
+	 */
+	
+	public void todoDisplay(List<Todo> todos) {
+		
+	}
+}
