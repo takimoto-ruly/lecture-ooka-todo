19d94ae925a57e9f580f67115efd89c5100756a6
takimoto-ruly <shoji.takimoto@ruly.jp>
2026-04-23 10:45:03 +0900

    Calculatorのスケルトンとテストコードを追加

---
+++ src/main/java/ruly/lecture/ooka/practice/basic/Calculator.java
@@ -0,0 +1,11 @@
+package ruly.lecture.ooka.practice.basic;
+
+public class Calculator {
+	/**
+	 * 1からmaxまでの合計を算出する
+	 */
+	public int sumUp(int max) {
+		// TODO: 合計を保持する変数を用意し、for文で加算してください
+		return 0;
+	}
+}
