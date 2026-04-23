d88c0038fa98459a7a3d3cbc1a9df10c32f5dc7e
ruly-ooka <ayana.ruly@outlook.jp>
2026-04-23 11:01:18 +0900

    Calculaterメソッドの追加

--- src/main/java/ruly/lecture/ooka/practice/basic/Calculator.java
+++ src/main/java/ruly/lecture/ooka/practice/basic/Calculator.java
@@ -5,7 +5,11 @@ public class Calculator {
 	 * 1からmaxまでの合計を算出する
 	 */
 	public int sumUp(int max) {
+		int total = 0;
+		for (int i = 1; i <= max; i++) {
+			total += i;
+		}
 		// TODO: 合計を保持する変数を用意し、for文で加算してください
-		return 0;
+		return total;
 	}
 }
