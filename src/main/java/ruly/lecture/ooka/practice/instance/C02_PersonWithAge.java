b021f372bce3280c6131474b77db0bec900bf325
ruly-ooka <ayana.ruly@outlook.jp>
2026-04-27 10:18:15 +0900

    C02_PersonWithAgeクラスの実装

--- src/main/java/ruly/lecture/ooka/practice/instance/C02_PersonWithAge.java
+++ src/main/java/ruly/lecture/ooka/practice/instance/C02_PersonWithAge.java
@@ -4,15 +4,24 @@ package ruly.lecture.ooka.practice.instance;
  * 年齢管理機能付きのPersonクラス。
  */
 public class C02_PersonWithAge {
+	String name;
+	int age;
 
 	/**
 	 * 【仕様】<br>
 	 * 名前と年齢を受け取って初期化するコンストラクタを実装すること。
 	 */
+	public C02_PersonWithAge(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 
 	/**
 	 * 【仕様】<br>
 	 * 呼び出すと年齢(age)が1つ増える celebrateBirthday メソッドを実装すること。
 	 */
+	public void celebrateBirthday() {
+		this.age += 1;
+	}
 
 }
