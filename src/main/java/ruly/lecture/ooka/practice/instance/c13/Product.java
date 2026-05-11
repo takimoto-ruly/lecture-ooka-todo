e4532a99efcbdba5635279336c8e91b06e9f7a8e
takimoto-ruly <shoji.takimoto@ruly.jp>
2026-05-11 10:14:17 +0900

    課題13を追加

---
+++ src/main/java/ruly/lecture/ooka/practice/instance/c13/Product.java
@@ -0,0 +1,10 @@
+package ruly.lecture.ooka.practice.instance.c13;
+
+/**
+ * 商品情報を保持するクラスです。<br>
+ * フィールド: String name, int stockQuantity<br>
+ * コンストラクタ: 全フィールドを初期化
+ */
+public class Product {
+
+}
---
+++ src/main/java/ruly/lecture/ooka/practice/instance/c13/PurchaseOrder.java
@@ -0,0 +1,10 @@
+package ruly.lecture.ooka.practice.instance.c13;
+
+/**
+ * 注文内容を保持するクラスです。<br>
+ * フィールド: int orderQuantity<br>
+ * コンストラクタ: 全フィールドを初期化
+ */
+public class PurchaseOrder {
+
+}
