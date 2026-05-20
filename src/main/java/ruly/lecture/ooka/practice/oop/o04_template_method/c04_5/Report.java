fd0b703969aeaeead6542d18213169d88bc8476d
ruly-ooka <ayana.ruly@outlook.jp>
2026-05-20 16:23:53 +0900

    c04_5クラスの修正

--- src/main/java/ruly/lecture/ooka/practice/oop/o04_template_method/c04_5/Report.java
+++ src/main/java/ruly/lecture/ooka/practice/oop/o04_template_method/c04_5/Report.java
@@ -2,7 +2,7 @@ package ruly.lecture.ooka.practice.oop.o04_template_method.c04_5;
 
 abstract class Report {
 	public final String generateReport() {
-		return headerText() + "\n" + mainText() + "/n" + footerText();
+		return headerText() + "\n" + mainText() + "\n" + footerText();
 	}
 
 	protected abstract String headerText();
---
+++ src/main/java/ruly/lecture/ooka/practice/oop/o05_strategy/o05_1/Strategy.java
@@ -0,0 +1,6 @@
+package ruly.lecture.ooka.practice.oop.o05_strategy.o05_1;
+
+public interface Strategy {
+
+	abstract String peyMent();
+}
