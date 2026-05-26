38d60a06a960553b96e2431ee70d1ae2d9a0caae
ruly-ooka <ayana.ruly@outlook.jp>
2026-05-26 10:07:28 +0900

    o05_08クラスの修正

--- src/main/java/ruly/lecture/ooka/practice/oop/o05_strategy/advanced/o05_08/NormalFightingStyle.java
+++ src/main/java/ruly/lecture/ooka/practice/oop/o05_strategy/advanced/o05_08/NormalFightingStyle.java
@@ -1,9 +1,8 @@
 package ruly.lecture.ooka.practice.oop.o05_strategy.advanced.o05_08;
 
 public class NormalFightingStyle implements FightingStyleStrategy {
-
 	@Override
-	public int calculateAttockPower(int attackPower) {
+	public int calculateAttackPower(int attackPower) {
 		return attackPower;
 	}
 }
